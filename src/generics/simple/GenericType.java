89d4446672e3a0a34e53fa685b8df0a90089bac6
Valery Emelyanov <emelva@rambler.ru>
2020-07-01 16:24:10 +0400

    A simple generic type example

---
+++ src/generics/simple/GenericType.java
@@ -0,0 +1,18 @@
+package generics.simple;
+
+public class GenericType<T> {
+
+    private T t;
+
+    public GenericType(T t) {
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
