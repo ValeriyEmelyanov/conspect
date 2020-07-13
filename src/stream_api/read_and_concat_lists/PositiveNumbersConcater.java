6efc5851a7aa717c762ec2d024a70e1d4d76182e
Valery Emelyanov <emelva@rambler.ru>
2020-07-13 17:37:04 +0400

    Adding stream API examples (improving previous)

--- src/stream_api/read_and_concat_lists/PositiveNumbersConcater.java
+++ src/stream_api/read_and_concat_lists/PositiveNumbersConcater.java
@@ -3,6 +3,8 @@ package stream_api.read_and_concat_lists;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,6 +16,8 @@ public class PositiveNumbersConcater {
         ArrayList<Integer> list2 = readArrayList(scanner);
 
         ArrayList<Integer> result = concatPositiveNumbers(list1, list2);
+        //ArrayList<Integer> result = concatPositiveNumbersWithFuction(list1, list2);
+        //ArrayList<Integer> result = concatPositiveNumbersWithPredicat(list1, list2);
 
         result.forEach(n -> System.out.print(n + " "));
     }
@@ -28,6 +32,20 @@ public class PositiveNumbersConcater {
     private static ArrayList<Integer> concatPositiveNumbers(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         return Stream
                 .concat(list1.stream().filter(n -> n >= 0), list2.stream().filter(n -> n >= 0))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private static ArrayList<Integer> concatPositiveNumbersWithFuction(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        Function<ArrayList<Integer>, Stream<Integer>> positive = list -> list.stream().filter(n -> n >= 0);
+        return Stream
+                .concat(positive.apply(list1), positive.apply(list2))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private static ArrayList<Integer> concatPositiveNumbersWithPredicat(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        Predicate<Integer> positive = n -> n >= 0;
+        return Stream
+                .concat(list1.stream().filter(positive), list2.stream().filter(positive))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
