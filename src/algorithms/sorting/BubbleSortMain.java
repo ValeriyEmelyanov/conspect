7e43b3f90aed72fef49a11dbeeea6c71314c85a2
Valeriy Emelyanov <emelva@rambler.ru>
2021-08-02 11:37:17 +0300

    Bubble sort 2

--- src/algorithms/sorting/BubbleSortMain.java
+++ src/algorithms/sorting/BubbleSortMain.java
@@ -8,6 +8,10 @@ public class BubbleSortMain {
                 Arrays.toString(
                         bubbleSort(new int[]{21, 23, 19, 30, 11, 28})
                 ));
+        System.out.println(
+                Arrays.toString(
+                        bubbleSort2(new int[]{21, 23, 19, 30, 11, 28})
+                ));
     }
 
     public static int[] bubbleSort(int[] array) {
@@ -18,6 +22,23 @@ public class BubbleSortMain {
                     tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] bubbleSort2(int[] array) {
+        int tmp;
+        boolean isSorted = false;
+        for (int i = 1; i < array.length && !isSorted; i++) {
+            isSorted = true;
+            for (int j = 1; j < array.length - i + 1; j++) {
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                    isSorted = false;
                 }
             }
         }
