00031a332b308a156c8cfda0d8f185791cac1127
Valeriy Emelyanov <emelva@rambler.ru>
2022-07-05 14:30:55 +0300

    Builder pattern example

---
+++ src/patterns/creational/builder_bankaccount/BankAccount.java
@@ -0,0 +1,105 @@
+package patterns.creational.builder_bankaccount;
+
+public class BankAccount {
+    private long accountNumber;
+    private String owner;
+    private String branch;
+    private double balance;
+    private double interestRate;
+
+    private BankAccount() {
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber=" + accountNumber +
+                ", owner='" + owner + '\'' +
+                ", branch='" + branch + '\'' +
+                ", balance=" + balance +
+                ", interestRate=" + interestRate +
+                '}';
+    }
+
+    public static class Builder {
+        private final long accountNumber;
+        private String owner;
+        private String branch;
+        private double balance;
+        private double interestRate;
+
+        public Builder(long accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public Builder withOwner(String owner) {
+            this.owner = owner;
+            return this;    // by returning the builder each time, we can create a fluent interface
+        }
+
+        public Builder atBranch(String branch) {
+            this.branch = branch;
+            return this;
+        }
+
+        public Builder openingBalance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public Builder atRate(double interestRate) {
+            this.interestRate = interestRate;
+            return this;
+        }
+
+        public BankAccount build() {
+            BankAccount account = new BankAccount();
+            account.accountNumber = this.accountNumber;
+            account.owner = this.owner;
+            account.branch = this.branch;
+            account.balance  = this.balance;
+            account.interestRate = this.interestRate;
+            return account;
+        }
+    }
+}
